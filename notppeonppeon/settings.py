70cf2f1538a020a8f63b0fecef3780c54e83bff3
SeoLanee <seolan25@gmail.com>
2025-03-03 23:19:11 +0900

    Add settings for email verification

--- notppeonppeon/settings.py
+++ notppeonppeon/settings.py
@@ -65,9 +65,6 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-}
-
-SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
@@ -102,6 +99,14 @@ SIMPLE_JWT = {
     'TOKEN_OBTAIN_SERIALIZER': 'authorization.serializers.CustomTokenObtainPairSerializer',
     "TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSerializer",
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 ROOT_URLCONF = 'notppeonppeon.urls'
 
